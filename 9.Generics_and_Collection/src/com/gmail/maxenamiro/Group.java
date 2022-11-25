96ca0bb0b37940e2674e64ac19710b71fd31e456
Maxenamiro <maxenamiro@gmail.com>
2022-11-25 22:34:08 +0200

    final

--- 9.Generics_and_Collection/src/com/gmail/maxenamiro/Group.java
+++ 9.Generics_and_Collection/src/com/gmail/maxenamiro/Group.java
@@ -1,61 +1,27 @@
 package com.gmail.maxenamiro;
 
-import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Objects;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Group {
-	private String groupName;
-	private final Student[] students;
 
-	public Group(String groupName, Student[] students) {
-		super();
-		this.groupName = groupName;
-		this.students = new Student[10];
-	}
+	private String groupName;
+	private List<Student> students = new ArrayList<>(10);
 
 	public Group() {
+	}
+
+	public Group(String groupName) {
 		super();
-		students = new Student[10];
+		this.groupName = groupName;
 	}
 
-	public void addStudent(Student student) throws GroupOverflowException {
-		for (int i = 0; i < students.length; i++) {
-			if (students[i] == null) {
-				students[i] = student;
-				return;
-			}
-		}
-		throw new GroupOverflowException("У цій группі немає місця для нового студента");
-	}
-
-	public Student searchStudentByLastName(String lastName) throws StudentNotFoundException {
-		for (int i = 0; i < students.length; i++) {
-			if (students[i] != null) {
-				if (students[i].getLastName().equals(lastName)) {
-					return students[i];
-				}
-			}
-		}
-		throw new StudentNotFoundException("Такого студента в цій группі немає");
-	}
-
-	public boolean removeStudentById(int id) {
-		for (int i = 0; i < students.length; i++) {
-			if (students[i] != null) {
-				if (students[i].getId() == id) {
-					students[i] = null;
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	public void sortStudentByLastName() {
-		Arrays.sort(students, Comparator.nullsFirst(new StudentLastNameComparator()));
-		for (int i = 0; i < students.length; i++) {
-			System.out.println(students[i]);
-		}
+	public Group(String groupName, List<Student> students) {
+		this.groupName = groupName;
+		this.students = students;
 	}
 
 	public String getGroupName() {
@@ -66,12 +32,99 @@ public class Group {
 		this.groupName = groupName;
 	}
 
-	public Student[] getStudents() {
+	public List<Student> getStudents() {
 		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+
+	public void addStudent(Student student) throws GroupOverflowException {
+
+		if (students.size() < 10) {
+			students.add(student);
+			student.setGroupName(this.groupName);
+			student.setId(students.size());
+			return;
+		}
+		throw new GroupOverflowException("Student " + student.getLastName() + "add error ");
+	}
+
+	public Student searchStudentByLastName(String lastName) throws StudentNotFoundException {
+		Student tempStudent = null;
+
+		for (Student student : students) {
+			if (student.getLastName().equals(lastName)) {
+				tempStudent = student;
+			}
+		}
+		if (tempStudent == null) {
+			throw new StudentNotFoundException("Couldn't find the student " + lastName);
+		}
+		return tempStudent;
+	}
+
+	public boolean removeStudentById1(int id) {
+
+		for (Student student : students) {
+			if (student.getId() == id) {
+				System.out.println("Student " + id + " deleted!");
+				students.remove(student);
+				return true;
+			}
+		}
+		System.out.println("Student " + id + " not foud");
+		return false;
+	}
+
+	public void sortStudentsByLastName() {
+		Collections.sort(students, Comparator.comparing(Human::getLastName));
+	}
+
+	public boolean checkStudentsSimilarity() {
+		for (int i = 0; i < students.size() - 1; i++) {
+			for (int j = i + 1; j < students.size(); j++) {
+				if (students.get(i).getLastName() != null && students.get(i).getName() != null
+						&& students.get(j).getLastName() != null && students.get(j).getName() != null
+						&& students.get(i).getLastName().equals(students.get(j).getLastName())
+						&& students.get(i).getName().equals(students.get(j).getName())) {
+
+					System.out
+							.println("Same student " + students.get(i).getName() + " " + students.get(i).getLastName());
+					return true;
+				}
+			}
+		}
+		System.out.println("No equal student");
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(groupName, students);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Group other = (Group) obj;
+		return Objects.equals(groupName, other.groupName) && Objects.equals(students, other.students);
 	}
 
 	@Override
 	public String toString() {
-		return "Group [groupName=" + groupName + ", students=" + Arrays.toString(students) + "]";
+		sortStudentsByLastName();
+		StringBuilder studentsList = new StringBuilder("Group info: " + groupName + '\n');
+
+		for (Student student : students) {
+			studentsList.append(student.toString());
+		}
+		return studentsList.toString();
 	}
 }
--- 9.Generics_and_Collection/src/com/gmail/maxenamiro/Main.java
+++ 9.Generics_and_Collection/src/com/gmail/maxenamiro/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		ArrayList<String> group = new ArrayList<String>();
-		//group.add("Taras", "Shevchenko", Gender.MALE, 1, "Writer");
+		// ArrayList<String> group = new ArrayList<String>();
+		// group.add("Taras", "Shevchenko", Gender.MALE, 1, "Writer");
 		Student student1 = new Student("Taras", "Shevchenko", Gender.MALE, 1, "Writer");
 		Student student2 = new Student("Ivan", "Franko", Gender.MALE, 2, "Writer");
 		Student student3 = new Student("Lesya", "Ukrainka", Gender.FEMALE, 3, "Writer");
@@ -19,36 +19,49 @@ public class Main {
 		Student student9 = new Student("Volodymyr", "Vinnichenko", Gender.MALE, 9, "Writer");
 		Student student10 = new Student("Oleksandr", "Dovzhenko", Gender.MALE, 10, "Writer");
 
-		Group group1 = new Group();
-		group1.setGroupName("Writer");
+		Group a = new Group("Writer");
 
 		try {
-			group1.addStudent(student1);
-			group1.addStudent(student2);
-			group1.addStudent(student3);
-			group1.addStudent(student4);
-			group1.addStudent(student5);
-			group1.addStudent(student6);
-			group1.addStudent(student7);
-			group1.addStudent(student8);
-			group1.addStudent(student9);
-			//group1.addStudent(student10);
-			group1.searchStudentByLastName("Dovzhenko");
+			a.addStudent(student1);
+			a.addStudent(student2);
+			a.addStudent(student3);
+			a.addStudent(student4);
+			a.addStudent(student5);
+			a.addStudent(student6);
+			a.addStudent(student7);
+			a.addStudent(student8);
+			a.addStudent(student9);
+			a.addStudent(student10);
+		} catch (GroupOverflowException e) {
+			e.printStackTrace();
+		}
 
-		} catch (GroupOverflowException e) {
-			e.printStackTrace();
-		} catch (StudentNotFoundException r) {
-			r.printStackTrace();
-		}
-		
-		ReadingCharacters read = new ReadingCharacters();
 		try {
-			group1.addStudent(read.readingCharacters());
-		} catch (GroupOverflowException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println(a.searchStudentByLastName("Vinnichenko"));
+			System.out.println(a.searchStudentByLastName(student5.getLastName()));
+			System.out.println();
+
+		} catch (StudentNotFoundException e) {
+			System.out.println(e.getMessage());
 		}
-		group1.sortStudentByLastName();
+
+		a.sortStudentsByLastName();
+		System.out.println("Sorted group: " + a.getGroupName());
+		System.out.println();
+		System.out.println(a);
+
+		System.out.println("St 1 equals st 2 = " + student2.equals(student1));
+		System.out.println("St 2 equals st 3 = " + student2.equals(student3));
+		System.out.println("St 2 equals st 2 = " + student2.equals(student2));
+		System.out.println();
+
+		System.out.println(student1.getName() + " " + student1.getLastName() + " - hashCode = " + student1.hashCode());
+		System.out.println(student2.getName() + " " + student2.getLastName() + " - hashCode = " + student2.hashCode());
+		System.out.println(student3.getName() + " " + student3.getLastName() + " - hashCode = " + student3.hashCode());
+		System.out.println();
+
+		a.checkStudentsSimilarity();
+		System.out.println();
+
 	}
-
 }
